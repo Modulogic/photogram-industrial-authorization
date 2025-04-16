9fe8edef769de5a09d507a554a82281331b031f4
Modulogic <carlomodulo@uchicago.edu>
2025-04-16 17:29:15 +0000

    pt1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,14 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
 
+  before_action :ensure_current_user_is_owner, only [:update, :destroy, :edit]
+  def ensure_current_user_is_owner
+    if current_user == @photo.owner
+      redirect_back(fallback_location: root_url, alert: "You're not authprized for that.
+      ")
+    end
+  end
+  
   # GET /photos or /photos.json
   def index
     @photos = Photo.all
@@ -49,7 +57,7 @@ class PhotosController < ApplicationController
   end
 
   # DELETE /photos/1 or /photos/1.json
-  def destroy
+  def destroy   
     @photo.destroy
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Photo was successfully destroyed." }
